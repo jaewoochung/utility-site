3ae7cef3903dbc9a2356941d8ed215a9d9bcd769
Jaewoo Chung <jaewoochung77@gmail.com>
2021-10-20 19:24:59 -0400

    changed from UTC to EST

--- src/components/NbaSchedule.js
+++ src/components/NbaSchedule.js
@@ -31,8 +31,8 @@ const getDate2 = () => {
 	var utcTime = new Date();
 	var today = utcTime.toLocaleString('en-US', { timeZone: 'US/Eastern' }).split(',');
 	today = today[0].split('/');
-
-	var dayOfMonth = today[1]
+	
+	var dayOfMonth = (+today[1]) + 1
 	if (dayOfMonth < 10) {
 		dayOfMonth = "0" + dayOfMonth;
 	}
@@ -61,18 +61,17 @@ function NbaSchedule() {
 	
 	useEffect(() => {
 		const date1 = getDate1();
-		const date2 = getDate2();
 		
 		axios
-			.get("https://v1.basketball.api-sports.io/games?date="+date1+"&league=12&season=2021-2022", {
+			.get("https://v1.basketball.api-sports.io/games?date="+date1+"&league=12&season=2021-2022&timezone=America/New_York", {
 				"headers": {
 					"x-rapidapi-host": "v1.basketball.api-sports.io",
-					"x-rapidapi-key": "9d9a715447e2a3dbbeebac5d5297ce60"	
+					"x-rapidapi-key": "9d9a715447e2a3dbbeebac5d5297ce60"
 				}
 			})
 			.then(response => {
 				console.log(response.data.response);
-				setGames(response.data.response);
+				setGames(nbaGames.concat(response.data.response))
 			});
 	}, []);
 
